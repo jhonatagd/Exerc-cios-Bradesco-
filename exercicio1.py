4d7be724ca22a794b4ab23fb908bfa8d7052422f
Jhonata Godoy <88396979+jhonatagd@users.noreply.github.com>
2024-03-26 17:41:34 +0000

    subindo lc

---
+++ exercicio1.py
@@ -0,0 +1,7 @@
+numero = float(input("digite um numero: "))
+resto_divisao = numero %2
+
+if resto_divisao == 0:
+    print("numero digitado é par")
+else:
+    print("numero digitado é impar") 
---
+++ exercício João.py
@@ -0,0 +1,5 @@
+preco = 100
+desconto = 100
+valor_fim = (preco * desconto) /100
+
+print("o tenis custa", valor_fim)
